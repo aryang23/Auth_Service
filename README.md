da760cccfbf38ea79920256291bb42356c415e14
Aryan Grover <aryang23@Aryans-MacBook-Pro.local>
2023-07-21 12:16:36 +0530

    Added Many-Many Relation between User and Roles

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 AUTH Microservice
 
 npx sequelize model:generate --name User --attributes email:string,password:string
+
+npx sequelize model:generate --name Role --attributes name:string
+
+allowNull: false
+
+1:N
+User have many roles
+A role will belong to many users
+
+After through
+
+npx sequelize db:migrate
+
--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
-
+const db = require('./models/index');
 
 const {PORT} = require('./config/serverConfig');
 const apiRoutes = require('./routes/index');
+
+// const {User, Role} = require('./models/index');
 
 const prepareAndStartServer = () => {
 
@@ -13,9 +15,17 @@ const prepareAndStartServer = () => {
     app.use(bodyParser.urlencoded({ extended: true }));
 
     app.use('/api', apiRoutes);
-    app.listen(PORT, () => {
+    app.listen(PORT, async () => {
         console.log(`Server started at port ${PORT}`);
+        if(process.env.DB_SYNC) {
+            db.sequelize.sync({alter: true});
+        }
+
+        // const u1 = await User.findByPk(4);
+        // const r1 = await Role.findByPk(2);
+
+        // u1.addRole(r1);
     })
 }
 
-prepareAndStartServer();
+prepareAndStartServer(); 
